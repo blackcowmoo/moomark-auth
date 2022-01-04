519af934b4635e98389e9e740e92a6c525a8b218
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-01-05 01:41:13 +0900

    Update JwtTokenProviderTest.java
    checkstyle을 위한 변경 사항 추가

--- src/test/java/com/blackcowmoo/moomark/auth/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/jwt/JwtTokenProviderTest.java
@@ -1,6 +1,7 @@
 package com.blackcowmoo.moomark.auth.jwt;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
@@ -20,8 +21,9 @@ public class JwtTokenProviderTest {
   public void testCreateRefreshToken() {
     String accessToken = jwtTokenProvider.createJwtRefreshToken("REFRESH_TOKEN");
     String givenValue = String.valueOf(jwtTokenProvider.getClaimsFromJwtToken(accessToken).getBody().get("value"));
-    if (!givenValue.equals("REFRESH_TOKEN"))
+    if (!givenValue.equals("REFRESH_TOKEN")) {
       fail("refresh token is error");
-    
+    }
+    assertEquals("REFRESH_TOKEN", givenValue);
   }
 }
