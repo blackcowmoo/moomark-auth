c15d73608fa184a00618dee8ad2c435a72e83313
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-01-09 00:49:33 +0900

    add response class
    response 관련 클래스 추가

---
+++ src/main/java/com/blackcowmoo/moomark/auth/model/dto/Response.java
@@ -0,0 +1,18 @@
+package com.blackcowmoo.moomark.auth.model.dto;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+
+	private ResponseHeader header;
+	private ObjectNode payload;
+}
