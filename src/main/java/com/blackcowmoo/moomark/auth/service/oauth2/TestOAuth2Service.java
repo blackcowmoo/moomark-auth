70bc9211aeb4c1f3adc0e5ea36a26278b0da1fed
GyeongSu Han <micalgenus@gmail.com>
2022-05-15 01:24:33 +0900

    Fix test provider (#9)
    * fix test provider
    
    * add ENVIRONMENT

--- src/main/java/com/blackcowmoo/moomark/auth/service/oauth2/TestOAuth2Service.java
+++ src/main/java/com/blackcowmoo/moomark/auth/service/oauth2/TestOAuth2Service.java
@@ -7,22 +7,31 @@ import com.blackcowmoo.moomark.auth.service.TokenService;
 import com.blackcowmoo.moomark.auth.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestOAuth2Service {
+
+  @Value("${environment}")
+  private String environment;
+
   @Autowired
   private UserService userService;
 
   @Autowired
   private TokenService tokenService;
 
+  public Boolean isTest(String code) {
+    return environment.equals("dev") && code.startsWith("test-");
+  }
+
   public Token login(String testCode) {
     String[] tokenStrings = testCode.split("-");
     String id = tokenStrings[1];
     User user = userService.getUserById(id, AuthProvider.TEST);
     if (user == null) {
-      user = userService.signUp(id, AuthProvider.GOOGLE, "test", "test@blackcowmoo.com",
+      user = userService.signUp(id, AuthProvider.TEST, "test", "test@blackcowmoo.com",
           "https://www.gravatar.com/avatar/HASH");
     }
 
