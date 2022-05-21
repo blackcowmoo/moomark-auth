ebf3a835c705eeb28e306d342f919889f7a14a42
micalgenus <micalgenus@gmail.com>
2022-05-22 01:58:34 +0900

    add testcode

---
+++ src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
@@ -0,0 +1,32 @@
+package com.blackcowmoo.moomark.auth.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.blackcowmoo.moomark.auth.model.oauth2.Token;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = OAuth2Controller.class)
+public class OAuth2ControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @Test
+  public void TEST_GOOGLE_CODE() throws Exception {
+    Token token = mapper
+        .readValue(mvc.perform(get("/api/v1/oauth2/google").param("code", "test-1234")).andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString(), Token.class);
+
+    assertNotNull(token.getToken());
+    assertNotNull(token.getRefreshToken());
+  }
+}
