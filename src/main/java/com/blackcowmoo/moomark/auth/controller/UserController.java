86d5aad9b7f03d8e3ea88dac66187a7d685a4157
micalgenus <micalgenus@gmail.com>
2022-05-22 00:54:30 +0900

    add delete user

--- src/main/java/com/blackcowmoo/moomark/auth/controller/UserController.java
+++ src/main/java/com/blackcowmoo/moomark/auth/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.blackcowmoo.moomark.auth.controller;
 
 import com.blackcowmoo.moomark.auth.model.entity.User;
+import com.blackcowmoo.moomark.auth.service.UserService;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/user")
 public class UserController {
+  private final UserService userService;
 
   private User getUser() {
     return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -18,5 +24,10 @@ public class UserController {
   @GetMapping
   public User getUserInfo() {
     return getUser();
+  }
+
+  @DeleteMapping
+  public void deleteUser() {
+    userService.withdraw(getUser());
   }
 }
