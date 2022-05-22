b08e5c6d04fe3e949e55f55297724704a8f3df1f
GyeongSu Han <micalgenus@gmail.com>
2022-05-22 19:09:41 +0900

    Add google oauth login test code (#11)
    * add testcode
    
    * order static import
    
    * update actions
    
    * change method name
    
    * fix actions
    
    * order static import
    
    * change method name
    
    * fix order
    
    * fix test
    
    * import order

---
+++ src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
@@ -0,0 +1,34 @@
+package com.blackcowmoo.moomark.auth.controller;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.blackcowmoo.moomark.auth.model.oauth2.Token;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OAuth2ControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @Test
+  public void testGoogleCode() throws Exception {
+    Token token = mapper
+        .readValue(mvc.perform(get("/api/v1/oauth2/google").param("code", "test-1234")).andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString(), Token.class);
+
+    assertNotNull(token.getToken());
+    assertNotNull(token.getRefreshToken());
+  }
+}
