b6c4aaa645e091e30965807cdf5bcc8edc74bfde
micalgenus <micalgenus@gmail.com>
2022-06-25 21:52:48 +0900

    remove unused import

--- src/main/java/com/blackcowmoo/moomark/auth/controller/PassportController.java
+++ src/main/java/com/blackcowmoo/moomark/auth/controller/PassportController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.blackcowmoo.moomark.auth.model.entity.User;
 import com.blackcowmoo.moomark.auth.service.PassportService;
