4fd25343150937dba59d58f0bca24d7a6a354e52
micalgenus <micalgenus@gmail.com>
2022-06-27 00:22:50 +0900

    change param

--- src/main/java/com/blackcowmoo/moomark/auth/controller/PassportController.java
+++ src/main/java/com/blackcowmoo/moomark/auth/controller/PassportController.java
@@ -2,17 +2,15 @@ package com.blackcowmoo.moomark.auth.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blackcowmoo.moomark.auth.model.entity.User;
 import com.blackcowmoo.moomark.auth.service.PassportService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1/passport")
@@ -29,7 +27,7 @@ public class PassportController {
   }
 
   @GetMapping("/verify")
-  public User verifyPassport(@RequestParam String passport) {
+  public User verifyPassport(@RequestHeader("x-moom-passport") String passport) {
     return passportService.parsePassport(passport);
   }
 
