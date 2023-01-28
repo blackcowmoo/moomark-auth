2d0d920a95cd809225aed2e8c00391b5f2dfe154
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-01-29 03:35:40 +0900

    Create AesUtilTest.java
    Create test code of AesUtil.java

---
+++ src/test/java/com/blackcowmoo/moomark/auth/util/AesUtilTest.java
@@ -0,0 +1,28 @@
+package com.blackcowmoo.moomark.auth.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AesUtilTest {
+
+
+  @Test
+  void encryptAndDecryptTest()
+    throws IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    String plainText = "PLAIN TEXT";
+
+    SecretKey aesSecretKey = AesUtil.generateNewKey();
+    byte[] encryptData = AesUtil.encrypt(plainText, aesSecretKey);
+    String decryptionText = AesUtil.decrypt(encryptData, aesSecretKey);
+
+    Assertions.assertThat(plainText).isEqualTo(decryptionText);
+  }
+}
