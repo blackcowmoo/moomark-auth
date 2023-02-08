df6b958a9cdab64eb4b195f8ff428bc71230d124
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-02-09 03:18:47 +0900

    [TEST] Create HashUtilsTest.java
    Create HashUtilsTest.java

---
+++ src/test/java/com/blackcowmoo/moomark/auth/util/HashUtilsTest.java
@@ -0,0 +1,18 @@
+package com.blackcowmoo.moomark.auth.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HashUtilsTest {
+  @Test
+  void Sha256Test() {
+    String testMsg = "TEST MESSAGE";
+    String testMsg2 = "TEST MESSAGE2";
+    String result = HashUtils.toSha256(testMsg);
+    String result2 = HashUtils.toSha256(testMsg2);
+
+    Assertions.assertThat(result).isNotEqualTo(result2);
+  }
+}
