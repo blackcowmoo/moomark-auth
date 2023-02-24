f2cc2d86caaa13f358dffffc9c0fd23f1f38c37b
Apisapple <kimminyong2034@gmail.com>
2023-02-25 01:31:17 +0900

    Update PassportKeyRedisRepositoryTest.java
    [REFACTOR] Delete unnecessary information

--- src/test/java/com/blackcowmoo/moomark/auth/repository/PassportKeyRedisRepositoryTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/repository/PassportKeyRedisRepositoryTest.java
@@ -2,7 +2,6 @@ package com.blackcowmoo.moomark.auth.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.blackcowmoo.moomark.auth.configuration.EmbeddedRedisConfig;
 import com.blackcowmoo.moomark.auth.model.entity.PassportKey;
 import com.blackcowmoo.moomark.auth.util.AesUtil;
 import com.blackcowmoo.moomark.auth.util.HashUtils;
@@ -11,10 +10,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
-import org.springframework.context.annotation.Import;
 
 @DataRedisTest
-@Import(EmbeddedRedisConfig.class)
 class PassportKeyRedisRepositoryTest {
 
   @Autowired
