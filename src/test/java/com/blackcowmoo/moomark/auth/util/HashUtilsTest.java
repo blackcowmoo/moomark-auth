5259f05ffc251b7e8e0efe787701c92b5df23e85
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-02-28 02:38:09 +0900

    Update HashUtilsTest.java
    [TEST] rename function

--- src/test/java/com/blackcowmoo/moomark/auth/util/HashUtilsTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/util/HashUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class HashUtilsTest {
 
   @Test
-  void Sha256Test() {
+  void sha256Test() {
     String testMsg = "TEST MESSAGE";
     String testMsg2 = "TEST MESSAGE2";
     String result = HashUtils.toSha256(testMsg);
