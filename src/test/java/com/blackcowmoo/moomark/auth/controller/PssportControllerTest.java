b3c85f62d43728e26540aa0eee360cd278279db5
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-03-02 01:14:51 +0900

    Add testcontainers
    [CORE][TEST] Add testcontainers

--- src/test/java/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.java
@@ -12,15 +12,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.blackcowmoo.moomark.auth.model.entity.User;
 import com.blackcowmoo.moomark.auth.model.oauth2.Token;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(
+  webEnvironment = WebEnvironment.RANDOM_PORT
+)
 @AutoConfigureMockMvc
+@Testcontainers
 public class PssportControllerTest {
+  @Container
+  private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:8.0.24")
+    .withUsername("root")
+    .withPassword("root")
+    .withInitScript("initDB.sql");
+
+
+  @DynamicPropertySource
+  public static void properties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+    registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+    registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+  }
+
   @Value("${passport.public-key}")
   private String passportPublicKey;
 
