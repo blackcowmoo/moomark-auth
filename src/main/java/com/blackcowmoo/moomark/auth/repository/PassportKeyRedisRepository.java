f5cead08067cdbdf3389f1f7d4d4f98804c9691e
GyeongSu Han <micalgenus@gmail.com>
2026-02-11 00:56:28 +0900

    fix: github-actions (#29)
    * fix actions
    
    * update actions
    
    * fix java distribution
    
    * fix tags
    
    * typo
    
    * fix agent
    
    * update env
    
    * fix secret
    
    * Revert "fix secret"
    
    This reverts commit d48b1e341c103b5ae2cecc17d947242f7f4bc44b.
    
    * Revert "update env"
    
    This reverts commit 301f1ce2a62db48ccd83994598b8d51663c0f996.
    
    * change registry
    
    * migrate service
    
    * add always
    
    * wait mysql
    
    * add docker.sock
    
    * gradle cleanup
    
    * Revert "Feature/feature add redis (#24)"
    
    This reverts commit 2dde3f760c62f34ce95356e077612cda6b7a00b4.
    
    * disable cache

--- src/main/java/com/blackcowmoo/moomark/auth/repository/PassportKeyRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blackcowmoo.moomark.auth.repository;
-
-import com.blackcowmoo.moomark.auth.model.entity.PassportKey;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PassportKeyRedisRepository extends CrudRepository<PassportKey, String> {
-
-}
