4c76177c0a181148b981516e71579a4f90c2edd6
blackcowmooo <blackcowmooo@gmail.com>
2026-02-16 09:35:17 +0000

    Update Jacoco task configuration to use simplified property assignment

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ jacoco {
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
-        xml.enabled.set(true)
-        html.enabled.set(true)
+        xml.enabled = true
+        html.enabled = true
     }
 }
