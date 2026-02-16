96ff76078c7078c32007ad603c129fc43ce26a0e
blackcowmooo <blackcowmooo@gmail.com>
2026-02-16 16:57:13 +0000

    Convert AuthApplication from Java to Kotlin

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/AuthApplication.kt
@@ -0,0 +1,11 @@
+package com.blackcowmoo.moomark.auth
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AuthApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AuthApplication::class.java, args)
+}
