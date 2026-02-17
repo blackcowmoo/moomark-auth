011cf93fd29173471189aa1c18201adcc0697df3
blackcowmooo <blackcowmooo@gmail.com>
2026-02-17 18:35:07 +0000

    Add imports for OAuth2 controller tests

--- src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.blackcowmoo.moomark.auth.model.oauth2.Token;
+import org.json.JSONObject;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
