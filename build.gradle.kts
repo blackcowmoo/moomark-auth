620e949afcf7065ab672c0eb97d7bb5f2e74fd83
blackcowmooo <blackcowmooo@gmail.com>
2026-02-18 10:47:44 +0000

    Update sourceSets to use Kotlin DSL property syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,10 @@ repositories {
 
 sourceSets {
     main {
-        kotlin.srcDirs = listOf("src/main/kotlin")
+        kotlin.srcDirs.set(listOf("src/main/kotlin"))
     }
     test {
-        kotlin.srcDirs = listOf("src/test/kotlin")
+        kotlin.srcDirs.set(listOf("src/test/kotlin"))
     }
 }
 
