2318c8dfba937ce42927e70e7ad69184a5d6ce13
blackcowmooo <blackcowmooo@gmail.com>
2026-02-18 13:52:49 +0000

    make AuthApplication class open for Spring Boot subclassing

--- src/main/kotlin/com/blackcowmoo/moomark/auth/AuthApplication.kt
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/AuthApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class AuthApplication
+open class AuthApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(arrayOf(AuthApplication::class.java), args)
