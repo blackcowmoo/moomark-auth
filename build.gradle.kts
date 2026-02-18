2a0f1f702e786ae2939f67f62c4225c3aa57e825
blackcowmooo <blackcowmooo@gmail.com>
2026-02-18 04:19:32 +0000

    Update kotlin.srcDirs to use typed setter

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,10 @@ repositories {
 
 sourceSets {
     main {
-        kotlin.srcDirs("src/main/kotlin")
+        kotlin.srcDirs.set(listOf("src/main/kotlin"))
     }
     test {
-        kotlin.srcDirs("src/test/kotlin")
+        kotlin.srcDirs.set(listOf("src/test/kotlin"))
     }
 }
 
