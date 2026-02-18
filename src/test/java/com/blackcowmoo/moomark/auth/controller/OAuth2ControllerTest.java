07288d6ea900231c36114d6d594789128d50bc4b
blackcowmooo <blackcowmooo@gmail.com>
2026-02-18 18:11:41 +0000

    test: update @AutoConfigureMockMvc attribute from securityEnabled to secure

--- src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
+++ src/test/java/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.java
@@ -18,7 +18,7 @@ import org.json.JSONObject;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
-@AutoConfigureMockMvc(securityEnabled = false)
+@AutoConfigureMockMvc(secure = false)
 public class OAuth2ControllerTest {
   @Autowired
   private MockMvc mvc;
