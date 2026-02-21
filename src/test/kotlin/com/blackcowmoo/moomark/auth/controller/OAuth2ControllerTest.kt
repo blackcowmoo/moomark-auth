0232b7bfbea460122e4038db08a5da7d4638dd3a
blackcowmooo <blackcowmooo@gmail.com>
2026-02-21 10:16:31 +0000

    refactor(test): replace deprecated extension functions with MockMvcRequestBuilders
    - Replace get/post/put extension functions with MockMvcRequestBuilders.get/post/put

--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.kt
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
