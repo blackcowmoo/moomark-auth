4a9834dc9686c4db049a54aa07e5df6d76befa53
blackcowmooo <blackcowmooo@gmail.com>
2026-03-02 16:37:08 +0000

    Add kotlinAnnotationProcessor configuration for Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
+    kotlinAnnotationProcessor
 }
 
 repositories {
@@ -41,6 +42,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java:8.0.27")
     annotationProcessor("org.projectlombok:lombok")
+    kotlinAnnotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
