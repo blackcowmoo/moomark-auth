354cf8878429715b7313d7b08ae8ba3274d4a72d
blackcowmooo <blackcowmooo@gmail.com>
2026-03-04 18:34:55 +0000

    Add @Accessors(fluent = true) to domain models
    - Enable fluent accessor methods for User, GoogleTokenResponse, and GoogleTokenResult
    - Improve code readability and consistency

--- src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResult.java
+++ src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResult.java
@@ -3,8 +3,10 @@ package com.blackcowmoo.moomark.auth.model.oauth2;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(fluent = true)
 public class GoogleTokenResult {
   private String iss;
   private String azp;
