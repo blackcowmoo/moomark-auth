df186edd8e7bd9475c867c88265785480ba70832
blackcowmooo <blackcowmooo@gmail.com>
2026-03-04 16:15:46 +0000

    Refactor Lombok annotations and update version
    - Update Lombok to version 1.18.30 explicitly
    - Replace @Data with @Getter and @ToString in GoogleTokenResponse and GoogleTokenResult

--- src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResponse.java
+++ src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResponse.java
@@ -2,11 +2,13 @@ package com.blackcowmoo.moomark.auth.model.oauth2;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
 @NoArgsConstructor
+@ToString
 public class GoogleTokenResponse {
   @JsonProperty("access_token")
   private String accessToken;
--- src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResult.java
+++ src/main/java/com/blackcowmoo/moomark/auth/model/oauth2/GoogleTokenResult.java
@@ -2,11 +2,13 @@ package com.blackcowmoo.moomark.auth.model.oauth2;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
 @NoArgsConstructor
+@ToString
 public class GoogleTokenResult {
   private String iss;
   private String azp;
