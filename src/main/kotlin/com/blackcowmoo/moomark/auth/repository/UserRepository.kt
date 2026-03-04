980c920014b30c20c9a09e45351620f186228596
blackcowmooo <blackcowmooo@gmail.com>
2026-03-04 05:07:37 +0000

    migrate UserRepository from Java to Kotlin

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.blackcowmoo.moomark.auth.repository
+
+import com.blackcowmoo.moomark.auth.model.AuthProvider
+import com.blackcowmoo.moomark.auth.model.entity.User
+import com.blackcowmoo.moomark.auth.model.entity.UserId
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, UserId> {
+    fun findByIdAndAuthProvider(id: String, authProvider: AuthProvider): User?
+}
