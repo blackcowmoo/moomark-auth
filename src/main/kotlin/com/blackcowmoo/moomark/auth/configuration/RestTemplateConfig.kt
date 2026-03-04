422fe0ffa1ee7aa477ed5348c99fb53f80715060
blackcowmooo <blackcowmooo@gmail.com>
2026-03-04 13:53:43 +0900

    migrate RestTemplateConfig from Java to Kotlin (#47)
    * migrate RestTemplateConfig from Java to Kotlin
    
    * Make RestTemplateConfig class open for Spring proxying
    
    * Make restTemplate() method open for inheritance

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/configuration/RestTemplateConfig.kt
@@ -0,0 +1,13 @@
+package com.blackcowmoo.moomark.auth.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+open class RestTemplateConfig {
+    @Bean
+    open fun restTemplate(): RestTemplate {
+        return RestTemplate()
+    }
+}
