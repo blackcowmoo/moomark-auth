d594f6d828c824bfc5da4bda4118376320d1e317
blackcowmooo <blackcowmooo@gmail.com>
2026-03-05 01:57:43 +0000

    migrate User and UserId entities from Java to Kotlin
    Convert User and UserId entity classes from Java to Kotlin while maintaining JPA compatibility and equals/hashCode implementations

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/model/entity/User.kt
@@ -0,0 +1,66 @@
+package com.blackcowmoo.moomark.auth.model.entity
+
+import com.blackcowmoo.moomark.auth.model.AuthProvider
+import com.blackcowmoo.moomark.auth.model.Role
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Id
+import javax.persistence.IdClass
+
+@Entity
+@IdClass(UserId::class)
+class User : Serializable {
+    @Id
+    @Enumerated(EnumType.STRING)
+    var authProvider: AuthProvider? = null
+
+    @Id
+    var id: String? = null
+
+    @Column(nullable = false)
+    var email: String? = null
+
+    @Column
+    var nickname: String? = null
+
+    @Column
+    var picture: String? = null
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var role: Role? = null
+
+    constructor() {}
+
+    @JvmName("constructorWithParams")
+    constructor(id: String?, authProvider: AuthProvider?, email: String?, nickname: String?, picture: String?, role: Role?) {
+        this.id = id
+        this.email = email
+        this.nickname = nickname
+        this.picture = picture
+        this.role = role
+        this.authProvider = authProvider
+    }
+
+    fun updateNickname(nickname: String): User {
+        this.nickname = nickname
+        return this
+    }
+
+    fun updatePicture(picture: String): User {
+        this.picture = picture
+        return this
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || other !is User) return false
+        return id == other.id && authProvider == other.authProvider
+    }
+
+    override fun hashCode(): Int {
+        return java.util.Objects.hash(id, authProvider)
+    }
+}
