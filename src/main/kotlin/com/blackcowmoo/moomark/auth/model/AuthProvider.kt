4b6cb605adc61b1dacbf4e800e66b10509578f48
blackcowmooo <blackcowmooo@gmail.com>
2026-03-05 04:59:48 +0000

    refactor: migrate auth model classes from Java to Kotlin

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/model/AuthProvider.kt
@@ -0,0 +1,24 @@
+package com.blackcowmoo.moomark.auth.model
+
+import java.util.Arrays
+
+enum class AuthProvider(val key: String, val value: String) {
+    TEST("TEST", "테스트"),
+    GOOGLE("GOOGLE", "구글"),
+    GITHUB("GITHUB", "깃허브"),
+    EMPTY("EMPTY", "없음");
+
+    companion object {
+        @JvmStatic
+        fun getAuthProviderValue(provider: String): AuthProvider {
+            return Arrays.stream(AuthProvider.values())
+                .filter { it.hasAuthProvider(provider) }
+                .findAny()
+                .orElse(EMPTY);
+        }
+    }
+
+    fun hasAuthProvider(provider: String): Boolean {
+        return key == provider
+    }
+}
