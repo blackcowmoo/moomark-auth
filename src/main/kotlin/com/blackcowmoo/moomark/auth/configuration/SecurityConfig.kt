035f3f7a7dbd26fa3aa58648eddf3c22bf05d1b2
blackcowmooo <blackcowmooo@gmail.com>
2026-03-05 12:35:59 +0000

    migrate SecurityConfig from Java to Kotlin

---
+++ src/main/kotlin/com/blackcowmoo/moomark/auth/configuration/SecurityConfig.kt
@@ -0,0 +1,54 @@
+package com.blackcowmoo.moomark.auth.configuration
+
+import com.blackcowmoo.moomark.auth.configuration.oauth2.JwtAuthFilter
+import com.blackcowmoo.moomark.auth.configuration.oauth2.PassportFilter
+import com.blackcowmoo.moomark.auth.service.PassportService
+import com.blackcowmoo.moomark.auth.service.TokenService
+import com.blackcowmoo.moomark.auth.service.UserService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+class SecurityConfig(
+    private val tokenService: TokenService,
+    private val userService: UserService,
+    private val passportService: PassportService
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+            csrf { disable() }
+            formLogin { disable() }
+            httpBasic { disable() }
+            logout { disable() }
+            exceptionHandling {
+                authenticationEntryPoint = HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+            }
+            authorizeRequests {
+                antMatchers("/api/v1/oauth2/refresh").permitAll()
+                antMatchers("/api/v1/oauth2/google").permitAll()
+                antMatchers("/api/v1/user/{provider}/{userId}").permitAll()
+                antMatchers("/api/v1/passport/verify").permitAll()
+                antMatchers("/api/v1/passport/verify/public").permitAll()
+                antMatchers("/actuator/health").permitAll()
+                anyRequest().authenticated()
+            }
+        }
+        http.addFilterBefore(JwtAuthFilter(tokenService, userService), UsernamePasswordAuthenticationFilter::class.java)
+        http.addFilterBefore(PassportFilter(passportService), UsernamePasswordAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
