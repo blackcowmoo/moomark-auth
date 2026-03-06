6157293c972a7594d6eb03110bd038d62384c079
blackcowmooo <blackcowmooo@gmail.com>
2026-03-06 20:49:52 +0000

    test: add assertNotEquals import to AesUtilTest
    - Added import for assertNotEquals assertion utility

--- src/test/kotlin/com/blackcowmoo/moomark/auth/util/AesUtilTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/util/AesUtilTest.kt
@@ -2,6 +2,7 @@ package com.blackcowmoo.moomark.auth.util
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
