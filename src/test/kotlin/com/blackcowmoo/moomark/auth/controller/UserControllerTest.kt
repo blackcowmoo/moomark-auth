2a64932c34e528ee3b572a72c99e07abb4aa28f5
blackcowmooo <blackcowmooo@gmail.com>
2026-03-09 04:15:37 +0000

    Add @ActiveProfiles annotation to test classes and update test config
    - Add @ActiveProfiles('test') to OAuth2ControllerTest, PssportControllerTest, and UserControllerTest
    - Add environment: dev to application-test.yaml

--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/UserControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/UserControllerTest.kt
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
 
   @Value("\${resources.user.default-picture}")
