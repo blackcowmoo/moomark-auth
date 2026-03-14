c9668c648ccd70c778ce0f8272336bc69c4f320b
blackcowmooo <blackcowmooo@gmail.com>
2026-03-14 17:07:45 +0000

    Reformat AuthApplicationTests with better code style

--- src/test/kotlin/com/blackcowmoo/moomark/auth/AuthApplicationTests.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/AuthApplicationTests.kt
@@ -3,13 +3,15 @@ package com.blackcowmoo.moomark.auth
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(classes = [AuthApplication::class], properties = [
-  "spring.jpa.hibernate.ddl-auto=update"
-])
+@SpringBootTest(
+    classes = [AuthApplication::class],
+    properties = [
+        "spring.jpa.hibernate.ddl-auto=update"
+    ]
+)
 class AuthApplicationTests {
 
   @Test
   fun contextLoads() {
   }
-
 }
