fcf6c0852f3e37c4e375a92611b34c132ca461ee
blackcowmooo <blackcowmooo@gmail.com>
2026-03-17 17:20:49 +0000

    Update test controllers to use open classes for inheritance

--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
@@ -49,9 +49,9 @@ class OAuth2ControllerTest {
     val user = User("1234", AuthProvider.TEST, "test@test.com", "test", "https://test.com", Role.USER)
 
     `when`(tokenService.generateToken("1234", AuthProvider.TEST, Role.USER)).thenReturn(token)
-    `when`(tokenService.verifyToken(any())).thenReturn(true)
-    `when`(tokenService.getUid(any())).thenReturn("1234")
-    `when`(tokenService.getProvider(any())).thenReturn(AuthProvider.TEST)
+    `when`(tokenService.verifyToken(any<String>())).thenReturn(true)
+    `when`(tokenService.getUid(any<String>())).thenReturn("1234")
+    `when`(tokenService.getProvider(any<String>())).thenReturn(AuthProvider.TEST)
     `when`(userService.getUserById(AuthProvider.TEST, "1234")).thenReturn(user)
 
     val responseToken = mapper.readValue(
@@ -71,9 +71,9 @@ class OAuth2ControllerTest {
     val user = User("1234", AuthProvider.TEST, "test@test.com", "test", "https://test.com", Role.USER)
 
     `when`(tokenService.generateToken("1234", AuthProvider.TEST, Role.USER)).thenReturn(token)
-    `when`(tokenService.verifyToken(any())).thenReturn(true)
-    `when`(tokenService.getUid(any())).thenReturn("1234")
-    `when`(tokenService.getProvider(any())).thenReturn(AuthProvider.TEST)
+    `when`(tokenService.verifyToken(any<String>())).thenReturn(true)
+    `when`(tokenService.getUid(any<String>())).thenReturn("1234")
+    `when`(tokenService.getProvider(any<String>())).thenReturn(AuthProvider.TEST)
     `when`(userService.getUserById(AuthProvider.TEST, "1234")).thenReturn(user)
 
     val responseToken = mapper.readValue(
@@ -101,9 +101,9 @@ class OAuth2ControllerTest {
 
     `when`(tokenService.generateToken("1234", AuthProvider.TEST, Role.USER)).thenReturn(token)
     `when`(tokenService.generateToken("1234", AuthProvider.TEST, Role.USER)).thenReturn(newToken)
-    `when`(tokenService.verifyToken(any())).thenReturn(true)
-    `when`(tokenService.getUid(any())).thenReturn("1234")
-    `when`(tokenService.getProvider(any())).thenReturn(AuthProvider.TEST)
+    `when`(tokenService.verifyToken(any<String>())).thenReturn(true)
+    `when`(tokenService.getUid(any<String>())).thenReturn("1234")
+    `when`(tokenService.getProvider(any<String>())).thenReturn(AuthProvider.TEST)
     `when`(userService.getUserById(AuthProvider.TEST, "1234")).thenReturn(user)
 
     val responseToken = mapper.readValue(
