d62551b1962d7546e5458278ff2399491a5aaf2f
blackcowmooo <blackcowmooo@gmail.com>
2026-03-22 22:09:01 +0000

    test: remove unused Mockito.any import

--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/OAuth2ControllerTest.kt
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.assertj.core.api.Assertions.assertThat
 import org.json.JSONObject
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.any
 import org.mockito.Mockito.anyString
 import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
