dfe6e0fd1a05729cbe054b0a1c9f98900c565b4f
blackcowmooo <blackcowmooo@gmail.com>
2026-03-23 15:13:41 +0000

    Update passport parsing to handle non-null values

--- src/test/kotlin/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/controller/PssportControllerTest.kt
@@ -157,7 +157,7 @@ class PssportControllerTest {
       PassportResponse::class.java
     )
 
-    `when`(passportService.parsePassport(passport.passport, passport.key)).thenReturn(user3)
+    `when`(passportService.parsePassport(passport.passport!!, passport.key!!)).thenReturn(user3)
 
     val user4 = mapper.readValue(
       mvc.perform(
