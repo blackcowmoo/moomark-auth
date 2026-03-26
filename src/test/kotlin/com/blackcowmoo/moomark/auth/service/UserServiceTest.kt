1a68e05769c0a36ba77c2728938494877e6a1236
blackcowmooo <blackcowmooo@gmail.com>
2026-03-26 01:53:58 +0000

    test: add assertNotNull and assertNull imports to UserServiceTest

--- src/test/kotlin/com/blackcowmoo/moomark/auth/service/UserServiceTest.kt
+++ src/test/kotlin/com/blackcowmoo/moomark/auth/service/UserServiceTest.kt
@@ -1,6 +1,8 @@
 package com.blackcowmoo.moomark.auth.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.any
